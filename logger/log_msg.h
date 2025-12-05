457d11e1bef34f73f7931a195b9459491f7a0c85
Jackson <347960869@qq.com>
2025-12-05 21:29:16 +0800

    完善日志消息和日志源结构体

--- logger/log_msg.h
+++ logger/log_msg.h
@@ -6,7 +6,13 @@ namespace logger {
 
 /// @brief 日志信息
 struct LogMsg {
-    LogMsg(LogLevel lvl, LogSourceLoc& loc, StringView& msg) : level(lvl), location(std::move(loc)), message(std::move(msg)) {}
+    LogMsg(LogLevel lvl, LogSourceLoc loc, StringView msg) : level(lvl), location(loc), message(msg) {}
+
+    LogMsg(LogLevel lvl, StringView msg) : LogMsg(lvl, LogSourceLoc{}, msg) {}
+
+    LogMsg(const LogMsg& other) = default;
+
+    LogMsg& operator=(const LogMsg& other) = default;
 
     LogLevel level;
     LogSourceLoc location;
