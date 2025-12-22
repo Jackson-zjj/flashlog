e86b37649f5c5cb7afecdd47ab2f7ad161965d75
Jackson <347960869@qq.com>
2025-12-22 17:23:04 +0800

    新增proto序列化模块

--- src/gtest_third_party.cc
+++ src/gtest_third_party.cc
@@ -1,9 +1,27 @@
 #include "gtest/gtest.h"
 #include "fmt/core.h"
+#include "logger/proto/person.pb.h"
 
 TEST(ThirdPartyTest, FmtFunc) {
-    fmt::print("this is {} \n", "fmt print");
     EXPECT_EQ(fmt::format("Hello, {}", "World!"), "Hello, World!");
     EXPECT_EQ(fmt::format("1 + 2 = {}", 3), "1 + 2 = 3");
     EXPECT_EQ(fmt::format("{} * {} = {}", 0.5, 2, 1), "0.5 * 2 = 1");
+}
+
+TEST(ThirdPartyTest, ProtoFunc) {
+    Person person;
+    person.set_id(1);
+    person.set_name("Jackson");
+    person.set_email("jackson@example.com");
+
+    // 序列化到字符串
+    std::string cache;
+    EXPECT_TRUE(person.SerializeToString(&cache));
+
+    // 反序列化
+    Person new_person;
+    EXPECT_TRUE(new_person.ParseFromString(cache));
+    EXPECT_EQ(new_person.id(), 1);
+    EXPECT_EQ(new_person.name(), "Jackson");
+    EXPECT_EQ(new_person.email(), "jackson@example.com");
 }
